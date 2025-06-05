0155e2cfc970fdda3944bda54ce8402c5da0fdaa
250450992 <nayuta-masuda@kccs.co.jp>
2025-06-05 09:40:44 +0900

    初回コミット

---
+++ ObjStdEx0401/src/driver/Driver.java
@@ -0,0 +1,16 @@
+package driver;
+
+import store.SpecialProduct;
+
+public class Driver {
+	
+	public static void main(String[] args) {
+		
+		SpecialProduct keyboard = new SpecialProduct("特価キーボード",3600,4500);
+		
+		System.out.println();
+		keyboard.display();
+		keyboard.displayOrgPrice();
+	}
+	
+}
